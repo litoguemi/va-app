cc1619f1fd73bc8a3a680a76b67465ef4bcfede0
litoguemi <litoguemi_lw@hotmail.com>
2024-12-12 03:45:26 +0100

    Update Piechart.svelte
    adjustment width

--- src/components/Piechart.svelte
+++ src/components/Piechart.svelte
@@ -8,8 +8,8 @@
 
     // Margins and SVG dimensions
     let margins = { left: 30, top: 30, bottom: 40, right: 30 };
-    const width = 400;
-    const height = 300;
+    const width = 200;
+    const height = 200;
     const radius = Math.min(width, height) / 2 - Math.max(margins.left, margins.right);
     
     console.log('lengthPiegroupedData:'+groupedData.length);
